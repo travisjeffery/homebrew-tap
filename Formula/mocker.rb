617f219d5f5b5ffb7ca93f2297009f9693a34e93
travisjeffery <tj@travisjeffery.com>
2018-04-03 07:12:57 -0400

    mocker version 0.0.5

--- Formula/mocker.rb
+++ Formula/mocker.rb
@@ -1,9 +1,9 @@
 class Mocker < Formula
   desc "Mocker for Go."
   homepage "https://github.com/travisjeffery"
-  url "https://github.com/travisjeffery/mocker/releases/download/0.0.4/mocker_0.0.4_darwin_amd64.tar.gz"
-  version "0.0.4"
-  sha256 "30e6ee279dc6a4737ba82b6a477c37cef252aca06c2fcb3cd596b7bcbacd1ccd"
+  url "https://github.com/travisjeffery/mocker/releases/download/0.0.5/mocker_0.0.5_darwin_amd64.tar.gz"
+  version "0.0.5"
+  sha256 "3524199796e2f4ed03f2be312cd95e569f60d0d245257bcae0eb5d18ed4fda6d"
 
   def install
     bin.install "mocker"
