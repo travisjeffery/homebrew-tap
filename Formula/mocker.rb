778b9737729be094f6f725518b99f9371d76e7ca
travisjeffery <tj@travisjeffery.com>
2019-01-09 17:04:12 -0500

    Brew formula update for mocker version 1.0.0

--- Formula/mocker.rb
+++ Formula/mocker.rb
@@ -1,15 +1,11 @@
 class Mocker < Formula
   desc "Mocker for Go."
   homepage "https://github.com/travisjeffery"
-  url "https://github.com/travisjeffery/mocker/releases/download/0.0.6/mocker_0.0.6_darwin_amd64.tar.gz"
-  version "0.0.6"
-  sha256 "e439aabd898416a6960bdb7109c942a1d8744c72967330a6a0eaceafcc056143"
+  url "https://github.com/travisjeffery/mocker/releases/download/1.0.0/mocker_1.0.0_darwin_amd64.tar.gz"
+  version "1.0.0"
+  sha256 "616f6e5767605e2265d97b4fe6dd2c8c99c162710b2bed6f4103c0af632cf7b8"
 
   def install
     bin.install "mocker"
-  end
-
-  test do
-    
   end
 end
